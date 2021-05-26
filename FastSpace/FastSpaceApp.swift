26dc51600357c692bb32843f127a31627cb93d21
Layuntafelipe <flobo0701@icloud.com>
2021-05-26 19:27:09 -0300

    Initial Commit

---
+++ FastSpace/FastSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastSpaceApp.swift
+//  FastSpace
+//
+//  Created by Felipe Lobo on 26/05/21.
+//
+
+import SwiftUI
+
+@main
+struct FastSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
